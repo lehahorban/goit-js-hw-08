8a2849bb75b36678f9f5fd1075676997422c04d5
Oleksii Horban <lehagorban93@gmail.com>
2022-05-31 23:06:09 +0300

    npm

---
+++ src/js/01-gallery.js
@@ -0,0 +1,28 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+console.log(galleryItems);
+const galleryList = document.querySelector(".gallery")
+console.log(galleryList);
+console.log(galleryItems);
+
+function renderItem(galleryArray) {
+const markup = galleryArray.map(({preview, original, description }) =>
+    `<li>
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+   </li>`)
+    .join("")
+console.log(markup);
+galleryList.insertAdjacentHTML("afterbegin", markup)
+}
+renderItem(galleryItems)
+
+galleryList.addEventListener("click", showModal)
+
+function showModal(e) { 
+    e.preventDefault()
+    const lightbox = new SimpleLightbox('.gallery a', { captionsData: "alt", captionDelay: 250 });
+}
